72f94ba45749269f99e8a0cd29f4639fa5002bea
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-01-22 14:28:04 -0500

    updated with absolute path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BasicComponent } from './basic/basic.component';
+import { BasicComponent } from '@app/basic/basic.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'basic' },
   { path: 'basic', component: BasicComponent },
-  { path: 'lazy-load', loadChildren: './lazy-load/lazy-load.module#LazyLoadModule' },
-  { path: 'side-effects', loadChildren: './side-effects/side-effects.module#SideEffectsModule' },
-  { path: 'search', loadChildren: './search/search.module#SearchModule' },
-  { path: 'search2', loadChildren: './search2/search2.module#Search2Module' },
-  { path: 'crud', loadChildren: './crud/crud.module#CrudModule' },
-  { path: 'crud2', loadChildren: './crud2/crud2.module#Crud2Module' },
+  { path: 'lazy-load', loadChildren: '@app/lazy-load/lazy-load.module#LazyLoadModule' },
+  { path: 'side-effects', loadChildren: '@app/side-effects/side-effects.module#SideEffectsModule' },
+  { path: 'search', loadChildren: '@app/search/search.module#SearchModule' },
+  { path: 'search2', loadChildren: '@app/search2/search2.module#Search2Module' },
+  { path: 'crud', loadChildren: '@app/crud/crud.module#CrudModule' },
+  { path: 'crud2', loadChildren: '@app/crud2/crud2.module#Crud2Module' },
   { path: '**', pathMatch: 'full', component: BasicComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BasicModule } from './basic/basic.module';
-import { CoreModule } from './core/core.module';
-import { RootStoreModule } from './root-store';
+import { AppRoutingModule } from '@app/app-routing.module';
+import { AppComponent } from '@app/app.component';
+import { BasicModule } from '@app/basic/basic.module';
+import { RootStoreModule } from '@app/root-store';
+import { CoreModule } from '@core/core.module';
 
 @NgModule({
   declarations: [
