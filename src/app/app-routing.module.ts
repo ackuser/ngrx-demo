99246061942e04efc7f7bcf22db461643d70ecb5
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2020-04-06 18:09:32 -0400

    upgraded to angular 9.1.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,12 @@ import { BasicComponent } from '@app/basic/basic.component';
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'basic' },
   { path: 'basic', component: BasicComponent },
-  { path: 'lazy-load', loadChildren: '@app/lazy-load/lazy-load.module#LazyLoadModule' },
-  { path: 'side-effects', loadChildren: '@app/side-effects/side-effects.module#SideEffectsModule' },
-  { path: 'search', loadChildren: '@app/search/search.module#SearchModule' },
-  { path: 'search2', loadChildren: '@app/search2/search2.module#Search2Module' },
-  { path: 'crud', loadChildren: '@app/crud/crud.module#CrudModule' },
-  { path: 'crud2', loadChildren: '@app/crud2/crud2.module#Crud2Module' },
+  { path: 'lazy-load', loadChildren: () => import('@app/lazy-load/lazy-load.module').then(m => m.LazyLoadModule) },
+  { path: 'side-effects', loadChildren: () => import('@app/side-effects/side-effects.module').then(m => m.SideEffectsModule) },
+  { path: 'search', loadChildren: () => import('@app/search/search.module').then(m => m.SearchModule) },
+  { path: 'search2', loadChildren: () => import('@app/search2/search2.module').then(m => m.Search2Module) },
+  { path: 'crud', loadChildren: () => import('@app/crud/crud.module').then(m => m.CrudModule) },
+  { path: 'crud2', loadChildren: () => import('@app/crud2/crud2.module').then(m => m.Crud2Module) },
   { path: '**', pathMatch: 'full', component: BasicComponent }
 ];
 
